51d3bdd7aadf537b8df57f2c9bb43b328e6d2aa4
Steve Hiehn <stevehiehn@Steves-Mac-mini.hitronhub.home>
2022-12-28 21:51:41 -0800

    parsing delta times correctly

--- midi_query/midi_query.py
+++ midi_query/midi_query.py
@@ -5,7 +5,7 @@ from mido import tick2second, MidiFile, second2tick
 from mingus.containers import Note
 from mingus.core import chords
 
-TICKS_PER_BEAT = 480
+REQUIRED_NOTE_MATCH_PER_BAR = 3
 
 drum_filter = ['drum',
                'drums',
@@ -102,10 +102,10 @@ def get_last_note_start_time(midi_track):
     return final_note_start
 
 
-def group_messages_by_bar(track, track_length_ticks):
+def group_messages_by_bar(track, track_length_ticks, ticks_per_beat):
     bars = []
 
-    bar_length_ticks = TICKS_PER_BEAT * 4
+    bar_length_ticks = ticks_per_beat * 4
     print('bar_length_ticks: ' + str(bar_length_ticks))
 
     num_of_bars = int(track_length_ticks / bar_length_ticks)
@@ -120,11 +120,11 @@ def group_messages_by_bar(track, track_length_ticks):
         current_time = 0.0
         for msg in track:
             if 'time' in msg.dict():
-                if msg.type == 'note_on':
-                    if current_time >= start_time and current_time <= end_time:
-                        bar.append(msg)
-
                 current_time = current_time + msg.time
+
+                if msg.type == 'note_on':
+                    if current_time >= start_time and current_time < end_time:
+                        bar.append(msg)
 
         bars.append(bar)
 
@@ -146,45 +146,83 @@ def convert_messages_to_notes(bars_of_messages):
 
 def find_matches(target_progression, target_key, bars_of_notes):
     match_indexes = []
-    bar_matches = 0
+    bars_idx = 0
 
-    for bar_idx, bar in enumerate(bars_of_notes):
-        print('bar_idx: ' + str(bar_idx))
-        print('chord in target_prog: ' + str(target_progression[bar_matches]))
-        notes_in_chord = chords.from_shorthand(target_progression[bar_matches])
-        print('notes_in_target_prog_chord: ' + str(notes_in_chord))
-        print('notes_in_bar: ' + str(bar))
-
+    while bars_idx < len(bars_of_notes) - len(target_progression):
         notes_not_in_chord = []
-        note_chord_matches = []  # set maybe?
-        for note in bar:
-            name = note.name
-            if name in notes_in_chord:
-                if name not in note_chord_matches:
-                    note_chord_matches.append(name)
-            else:
-                notes_not_in_chord.append(name)
+        is_match = True
 
-        if len(note_chord_matches) > 1:
-            bar_matches = bar_matches + 1
-        else:
-            bar_matches = 0
+        for prog_idx, prog_chord in enumerate(target_progression):
+            note_chord_matches = []
 
-        print('note_chord_matches: ' + str(note_chord_matches))
+            print('COMPARE PROG_IDX: ' + str(prog_idx) + ' to BAR_IDX: ' + str(bars_idx + prog_idx))
+            notes_in_prog_chord = chords.from_shorthand(prog_chord)
+            notes_at_bar_idx = []
+            for note in bars_of_notes[bars_idx + prog_idx]:
+                notes_at_bar_idx.append(note.name)
 
-        if bar_matches == len(target_progression):
-            match_indexes.append(bar_idx)
-            bar_matches = 0
+            print('NOTES_IN_PROG_CHORD: ' + str(notes_in_prog_chord))
+            print('NOTES_IN_BAR_IDX_CHORD: ' + str(notes_at_bar_idx))
+
+            for note in bars_of_notes[bars_idx + prog_idx]:
+                name = note.name
+                if name in notes_in_prog_chord:
+                    if name not in note_chord_matches:
+                        note_chord_matches.append(name)
+                else:
+                    notes_not_in_chord.append(name)
+
+            if len(note_chord_matches) < REQUIRED_NOTE_MATCH_PER_BAR:
+                is_match = False
+
+            print('MATCH STATUS: ' + str(is_match))
+
+        if is_match:
+            match_indexes.append(bars_idx)
+
+        bars_idx = bars_idx + 1
+
+    # match_indexes = []
+    # bar_matches = 0
+    #
+    # for bar_idx, bar in enumerate(bars_of_notes):
+    #     print('bar_idx: ' + str(bar_idx))
+    #     print('chord in target_prog: ' + str(target_progression[bar_matches]))
+    #     notes_in_chord = chords.from_shorthand(target_progression[bar_matches])
+    #     print('notes_in_target_prog_chord: ' + str(notes_in_chord))
+    #     print('notes_in_bar: ' + str(bar))
+    #
+    #     notes_not_in_chord = []
+    #     note_chord_matches = []  # set maybe?
+    #     for note in bar:
+    #         name = note.name
+    #         if name in notes_in_chord:
+    #             if name not in note_chord_matches:
+    #                 note_chord_matches.append(name)
+    #         else:
+    #             notes_not_in_chord.append(name)
+    #
+    #     if len(note_chord_matches) > 1:
+    #         bar_matches = bar_matches + 1
+    #     else:
+    #         bar_matches = 0
+    #
+    #     print('note_chord_matches: ' + str(note_chord_matches))
+    #
+    #     if bar_matches == len(target_progression):
+    #         match_indexes.append(bar_idx)
+    #         bar_matches = 0
 
     return match_indexes
 
 
 def print_midi_file_info(midi_file):
-    MidiFile.ticks_per_beat = TICKS_PER_BEAT
-
     target_key = 'C'
     target_progression = ['Cmaj7', 'Emin7', 'Dmin7', 'Cmaj7']
     midi_file = mido.MidiFile('test_assets/cmaj7_em7_dm7_cmaj7_bar5_key_c.MID', clip=True)
+
+    ticks_per_beat = midi_file.ticks_per_beat
+    print('ticks_per_beat: ' + str(ticks_per_beat))
 
     print(
         'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
@@ -199,7 +237,7 @@ def print_midi_file_info(midi_file):
     track_length = midi_file.length
     print('track_length: ' + str(track_length))
 
-    track_length_ticks = second2tick(track_length, TICKS_PER_BEAT, tempo_in_ms)
+    track_length_ticks = second2tick(track_length, ticks_per_beat, tempo_in_ms)
     print('track_length_ticks: ' + str(track_length_ticks))
 
     filtered_tracks = filter_drum_tracks(midi_file.tracks)
@@ -207,7 +245,7 @@ def print_midi_file_info(midi_file):
     merged_track = merge_tracks(filtered_tracks)
     print('AFTER MERGE msg_count: ' + str(len(merged_track)))
 
-    bars_of_messages = group_messages_by_bar(merged_track, track_length_ticks)
+    bars_of_messages = group_messages_by_bar(merged_track, track_length_ticks, ticks_per_beat)
 
     # print('bars_of_messages: ' + str(bars_of_messages))
 
@@ -217,40 +255,8 @@ def print_midi_file_info(midi_file):
     match = find_matches(target_progression, target_key, bars_of_notes)
     print('has_match: ' + str(match))
 
-    # for idx, msg in enumerate(merged_track):
-    #
-    #     if idx < 1000:
-    # print('MSG:' + str(msg))
-    # if 'channel' in msg.dict():
-    #     if msg.channel > 10:
-    #         # drop the drum track
-    #         print('DROP:' + str(msg))
-    #     else:
-    #         print('msg.channel:' + str(msg.channel))
-
-    # print('MERGED_TRACK_COUNT: ' + str(len(midi_file.tracks)))
-
-    # print('TEMPO:' + str(mid.tempo))
-    # get tempo in seconds
-    # seconds_per_quarter_note = seconds_per_quarter_note(mid.tempos[0].tempo)
-
-    # mid.tick2second()
-    # for msg in midi_file:
-    #     if msg.is_meta:
-    #         print('META: ' + str(msg.tempo))
-    # print('msg:' + str(msg))
-
-    # for i, track in enumerate(mid.tracks):
-    #     print('Track {}: {}'.format(i, track.name))
-    # for msg in track:
-    #     print(msg)
-    #
-    # for msg in midi_file.play():
-    #     port.send(msg)
-
 
 def main():
-    print('dude')
     midi_file = mido.MidiFile('datasets/multi_track_pop.mid', clip=True)
 
     print_midi_file_info(midi_file)
